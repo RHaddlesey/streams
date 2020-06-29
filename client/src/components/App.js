ca09606481e84f7269f7b82a677a7bd28ded7e64
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2020-06-29 13:40:47 +0100

    building routes scaffolding

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      Page One
-      <Link to="/pagetwo">Navigate to page 2</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <Link to="/">Navigate to page 1</Link>
-    </div>
-  );
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
@@ -36,11 +26,21 @@ export default App;
 
 // ROUTER EXAMPLE
 // const PageOne = () => {
-//   return <div>Page One</div>;
+//   return (
+//     <div>
+//       Page One
+//       <Link to="/pagetwo">Navigate to page 2</Link>
+//     </div>
+//   );
 // };
 
 // const PageTwo = () => {
-//   return <div>Page Two</div>;
+//   return (
+//     <div>
+//       Page Two
+//       <Link to="/">Navigate to page 1</Link>
+//     </div>
+//   );
 // };
 
 // const App = () => {
