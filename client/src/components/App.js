6a89e331c857c72d11306f95490295e237967f10
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2020-06-29 13:20:32 +0100

    usink Link tag to navigate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      Page One
+      <Link to="/pagetwo">Navigate to page 2</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <Link to="/">Navigate to page 1</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
+
+// ROUTER EXAMPLE
+// const PageOne = () => {
+//   return <div>Page One</div>;
+// };
+
+// const PageTwo = () => {
+//   return <div>Page Two</div>;
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <div>
+//           <Route path="/" exact component={PageOne} />
+//           <Route path="/pagetwo" component={PageTwo} />
+//         </div>
+//       </BrowserRouter>
+//     </div>
+//   );
+// };
